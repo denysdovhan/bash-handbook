e987d13eaeb72098648b41a8521e69ea18ae0f20
andrepolischuk <andre.polischuk@gmail.com>
2016-01-13 17:01:53 +0300

    Fix readme path

--- bin/cmd.js
+++ bin/cmd.js
@@ -4,12 +4,13 @@ var fs = require('fs');
 var obj = require('through2').obj;
 var pager = require('default-pager');
 var msee = require('msee');
+var join = require('path').join;
 
 var mseeOpts = {
   paragraphEnd: '\n\n'
 };
 
-fs.createReadStream(__dirname + '/README.md')
+fs.createReadStream(join(__dirname, '../README.md'))
   .pipe(obj(function (chunk, enc, cb) {
     this.push(msee.parse(chunk.toString(), mseeOpts));
     cb();
